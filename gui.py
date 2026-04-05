edab10c3bacaec839c0c9f00dc3c7360a2e1fc52
fov <fovikson@proton.me>
2026-04-05 17:55:02 +0200

    fix: simplify code

--- gui.py
+++ gui.py
@@ -8,12 +8,9 @@ def resource_path(relative_path):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), relative_path)
 
-
 def run(start_cb, stop_cb):  
 
     ctk.set_appearance_mode("dark")
-
-
 
     app = ctk.CTk()        
     app.geometry("700x700")
@@ -34,108 +31,40 @@ def run(start_cb, stop_cb):
     labelCommands = ctk.CTkLabel(app, text="Komendy które mam pisac: ")
     labelCommands.pack(pady=5)
 
+    def createRow():
+        name = ctk.CTkFrame(app)
+        name.pack(pady=10)
+        return name
 
+    rows = [createRow() for i in range(7)]
 
-    # Commands
+    def displayRow(row):
+        entry = ctk.CTkEntry(row, width=200)
+        entry.pack(side="left", padx=(0,5))
+        return entry
 
-    row1 = ctk.CTkFrame(app)
-    row1.pack(pady=10)
+    def packRow(row):
+        entry = ctk.CTkEntry(row, width=30)
+        entry.pack(side="left")
+        return entry
 
-    row2 = ctk.CTkFrame(app)
-    row2.pack(pady=10)
+    entryCommand = []
+    entrySmall = []
 
-    row3 = ctk.CTkFrame(app)
-    row3.pack(pady=10)
+    for i in range(7):
+        entryCommand.append(displayRow(rows[i]))
+        entrySmall.append(packRow(rows[i]))
 
-    row4 = ctk.CTkFrame(app)
-    row4.pack(pady=10)
-
-    row5 = ctk.CTkFrame(app)
-    row5.pack(pady=10)
-
-    row6 = ctk.CTkFrame(app)
-    row6.pack(pady=10)
-
-    row7 = ctk.CTkFrame(app)
-    row7.pack(pady=10)
-
-    # First row 
-    entryCommandOne = ctk.CTkEntry(row1, width=200)
-    entryCommandOne.pack(side="left", padx=(0,5))
-
-    entrySmallOne = ctk.CTkEntry(row1, width=30)
-    entrySmallOne.pack(side="left")
-
-    # Second row
-    entryCommandTwo = ctk.CTkEntry(row2, width=200)
-    entryCommandTwo.pack(side="left", padx=(0,5))
-
-    entrySmallTwo = ctk.CTkEntry(row2, width=30)
-    entrySmallTwo.pack(side="left")
-    
-    # Third row
-    entryCommandThree = ctk.CTkEntry(row3, width=200)
-    entryCommandThree.pack(side="left", padx=(0,5))
-
-    entrySmallThree = ctk.CTkEntry(row3, width=30)
-    entrySmallThree.pack(side="left")
-
-    # Fourth row
-    entryCommandFour = ctk.CTkEntry(row4, width=200)
-    entryCommandFour.pack(side="left", padx=(0,5))
-
-    entrySmallFour = ctk.CTkEntry(row4, width=30)
-    entrySmallFour.pack(side="left")
-
-    # Fifth row
-    entryCommandFive = ctk.CTkEntry(row5, width=200)
-    entryCommandFive.pack(side="left", padx=(0,5))
-
-    entrySmallFive = ctk.CTkEntry(row5, width=30)
-    entrySmallFive.pack(side="left")
-
-    # Sixth row
-    entryCommandSix = ctk.CTkEntry(row6, width=200)
-    entryCommandSix.pack(side="left", padx=(0,5))
-
-    entrySmallSix = ctk.CTkEntry(row6, width=30)
-    entrySmallSix.pack(side="left")
-
-    # Seventh row
-    entryCommandSeven = ctk.CTkEntry(row7, width=200)
-    entryCommandSeven.pack(side="left", padx=(0,5))
-
-    entrySmallSeven = ctk.CTkEntry(row7, width=30)
-    entrySmallSeven.pack(side="left")
     def on_start():
         try:
-            
+            commands = [entryCommand[i].get() for i in range(7)]
 
-
-            commands = [
-                entryCommandOne.get(),
-                entryCommandTwo.get(),
-                entryCommandThree.get(),
-                entryCommandFour.get(),
-                entryCommandFive.get(),
-                entryCommandSix.get(),
-                entryCommandSeven.get()
-            ]
-
-            laps = [
-                 entrySmallOne.get(),
-                 entrySmallTwo.get(),
-                 entrySmallThree.get(),
-                 entrySmallFour.get(),
-                 entrySmallFive.get(),
-                 entrySmallSix.get(),
-                 entrySmallSeven.get(),
-            ]
-
+            laps = [entrySmall[i].get() for i in range(7)]
            
             blocks = int(entry.get())
             eating = int(entryFood.get())
             start_cb(blocks, commands, laps, eating)
+
         except ValueError:
             print("Błąd")
 
